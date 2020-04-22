e280e5ff7473edcf63cb898c3e6c15a9d5506533
ashakirzianov <ashakirzianov@icloud.com>
2020-04-22 17:47:04 -0300

    Initial

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    "space-before-function-paren": "off",
+    "indent": ["error", 4],
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"],
+  },
+}
