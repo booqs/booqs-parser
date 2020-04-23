99a2c8de995183feebbe395c3718bb54629fbfb4
ashakirzianov <ashakirzianov@icloud.com>
2020-04-23 15:25:49 -0300

    'genResult' & 'genAsyncResult'

--- src/result.ts
+++ src/result.ts
@@ -12,3 +12,35 @@ export type Failure = {
 };
 
 export type Result<T> = Success<T> | Failure;
+
+export type ResultGen<T> = Generator<Diagnostic, T | undefined>;
+export function genResult<T>(gen: ResultGen<T>): Result<T> {
+    const diags: Diagnostic[] = [];
+    let next = gen.next();
+    while (!next.done) {
+        diags.push(next.value);
+        next = gen.next();
+    }
+    const last = next.value;
+    if (last) {
+        return { value: last, diags };
+    } else {
+        return { diags };
+    }
+}
+
+export type ResultAsyncGen<T> = AsyncGenerator<Diagnostic, T | undefined>;
+export async function genAsyncResult<T>(gen: ResultAsyncGen<T>): Promise<Result<T>> {
+    const diags: Diagnostic[] = [];
+    let next = await gen.next();
+    while (!next.done) {
+        diags.push(next.value);
+        next = await gen.next();
+    }
+    const last = next.value;
+    if (last) {
+        return { value: last, diags };
+    } else {
+        return { diags };
+    }
+}
