dddb5fe15b3504a823f4ad67b21ceaf747bb7579
ashakirzianov <ashakirzianov@icloud.com>
2020-04-27 16:47:50 -0300

    Minor api fixes

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,20 @@ export async function parseMetadata(filePath: string) {
         return { diags };
     }
     const metadata = await getMetadata(epub);
+    return {
+        value: metadata,
+        diags,
+    };
+}
+
+export async function parseCover(filePath: string) {
+    const diags: Diagnostic[] = [];
+    const { value: epub, diags: fileDiags } = await openEpub({ filePath });
+    diags.push(...fileDiags);
+    if (!epub) {
+        return { diags };
+    }
+    const metadata = await getMetadata(epub);
     const coverHref = metadata.cover;
     if (typeof coverHref === 'string') {
         const coverBuffer = await epub.imageResolver(coverHref);
@@ -34,6 +48,7 @@ export async function parseMetadata(filePath: string) {
             diags.push({
                 diag: `couldn't load cover image: ${coverHref}`,
             });
+            return { value: null, diags };
         } else {
             const base64 = Buffer.from(coverBuffer).toString('base64');
             return {
@@ -44,12 +59,7 @@ export async function parseMetadata(filePath: string) {
                 diags,
             };
         }
+    } else {
+        return { value: null, diags };
     }
-    return {
-        value: {
-            cover: undefined,
-            meta: metadata,
-        },
-        diags,
-    };
 }
